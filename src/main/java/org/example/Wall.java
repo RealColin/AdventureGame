cf10dc81b48f199119c360df6948ed214f2f972d
Werner, Colin <cwerner001@student.franciscan.edu>
2025-04-22 09:43:39 -0400

    Cleanup Wall

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -1,12 +1,8 @@
 package org.example;
 
 public record Wall(int xPos, int yPos, int width, int height, int color) {
-
-    // Check if hitbox is inside this wall
+    // Check if the hitbox is inside this wall
     public boolean isInside(int x, int y, int w, int h) {
-        if (x < xPos+width && x+w > xPos && y < yPos+height && y+h > yPos)
-            return true;
-
-        return false;
+        return x < xPos + width && x + w > xPos && y < yPos + height && y + h > yPos;
     }
 }
